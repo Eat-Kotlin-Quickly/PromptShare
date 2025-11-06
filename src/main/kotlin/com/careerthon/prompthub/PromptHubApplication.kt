936188b2fdb6d8d8bddecb54ec24704fecc8ee4b
ican0422 <ican0422@gmail.com>
2025-11-06 22:26:08 +0900

    init

---
+++ src/main/kotlin/com/careerthon/prompthub/PromptHubApplication.kt
@@ -0,0 +1,11 @@
+package com.careerthon.prompthub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PromptHubApplication
+
+fun main(args: Array<String>) {
+    runApplication<PromptHubApplication>(*args)
+}
---
+++ src/test/kotlin/com/careerthon/prompthub/PromptHubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.careerthon.prompthub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PromptHubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
